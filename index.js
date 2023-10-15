a2131379d7e28b4ed85dc0a29ca49f46ade0d037
IfeomaBlessing <ifeomaobo92@gmail.com>
2023-10-15 13:34:53 +0100

    commit

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ let notify =document.getElementById("notify")
 let mark =document.getElementById("mark")
 let show =document.querySelector(".show")
 let hide =document.querySelector(".hide")
-let red = document.querySelector(".red")
 let unreadMessages =document.querySelectorAll(".unread");
 
 show.addEventListener("click",function(){
-  //  hide.style.display ="block";
    hide.classList.toggle("hide")
-
 })
 
 notify.innerText = unreadMessages.length;
@@ -17,8 +14,6 @@ unreadMessages.forEach((unreadMessage)=>{
     unreadMessage.addEventListener("click",function(){
     
       unreadMessage.classList.toggle("unread");
-      
-      red.classList.add("red")
   
       const newMessages= document.querySelectorAll(".unread");
   
@@ -33,7 +28,7 @@ unreadMessages.forEach((unreadMessage)=>{
   mark.innerText = "Mark All As Read"
   unreadMessages.forEach((unreadMessage)=>{
       mark.addEventListener("click",function(){
-        red.classList.add("dot") 
+        
        unreadMessage.classList.remove("unread") 
        hide.classList.remove("hide")
        
